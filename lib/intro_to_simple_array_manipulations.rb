04545cf3b63a668da1c08a758ee521834aa33cf9
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-08 19:38:10 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,7 +4,5 @@ def using_push(countries_in_western_africa,next_country)
   countries_in_western_africa.push(next_country)
 end
 def using_unshift(neighborhoods_in_northwest_brooklyn,@new_neighborhood)
-  neighborhoods_in_northwest_brooklyn =  ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  @new_neighborhood = "Brooklyn Heights"
-  neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+
 end
