3d8f17c89d3d2c1717ddd6a8f1323ce155aa971d
Theodore Kimana <kimana.theodore@gmail.com>
2019-02-08 19:33:08 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,13 @@
+def using_push(countries_in_western_africa,next_country)
+  countries_in_western_africa = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  next_country = "Niger"
+  countries_in_western_africa.push(next_country)
+end
+def using_unshift(neighborhoods_in_northwest_brooklyn,@new_neighborhood)
+  neighborhoods_in_northwest_brooklyn =  ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  @new_neighborhood = "Brooklyn Heights"
+  neighborhoods_in_northwest_brooklyn.unshift(@new_neighborhood)
+end
+def using_unshift(neighborhoods_in_northwest_brookly,@new_neighborhood)
+  
+  
